7dd483408518f851ec348ddf1bcb6ff65b4c57a5
Kazuki Tobita <kazukiti201@gmail.com>
2018-03-17 04:33:57 +0900

    create get command

--- codic.js
+++ codic.js
@@ -3,6 +3,8 @@
 'use strict'
 
 const fsExtra = require('fs-extra')
+const request = require('request');
+
 const args = process.argv.slice(2)
 
 if (args[0] === 'set') {
@@ -13,4 +15,20 @@ if (args[0] === 'set') {
         encoding: 'utf-8'
     })
     console.log(args[1])
+}
+
+if (args[0] === 'get') {
+    request.get({
+        url: 'https://api.codic.jp/v1/engine/translate.json',
+        headers: {
+            'Authorization': 'Bearer test'
+        },
+        qs: {
+            text: '得点を取得する',
+            casing: 'camel'
+        },
+        json: true
+    }, (err, req, data) => {
+        console.log(data[0].translated_text)
+    })
 }
