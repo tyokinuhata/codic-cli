bcbb7a337d2a7cf5916180c17e03e6b5c5d54494
Kazuki Tobita <kazukiti201@gmail.com>
2018-03-19 06:56:34 +0900

    update index

--- index.js
+++ index.js
@@ -8,34 +8,34 @@ const pkg = require(__dirname + '/package.json')
 
 const args = process.argv.slice(2)
 
-/** アクセストークンの設定 */
-if (args[0] === 'conf' && args[1] === 'token') {
-    const config = fsExtra.readJSONSync(__dirname + '/codic-config.json')
-    config['access-token'] = args[2]
-    fsExtra.writeJSONSync(__dirname + '/codic-config.json', config)
+/** 設定 */
+const conf = {
+    token: (file, token) => {
+        const config = fsExtra.readJSONSync(file)
+        config['access-token'] = token
+        fsExtra.writeJSONSync(file, config)
+    },
+    casing: (file, casing) => {
+        const config = fsExtra.readJSONSync(file)
+        config.casing = casing
+        fsExtra.writeJSONSync(file, config)
+    },
+    list: file => {
+        const config = fsExtra.readJsonSync(file)
+        console.log('access-token: ' + config['access-token'])
+        console.log('casing: ' + config.casing)
+    }
+}
 
-/** 命名規則の設定 */
-} else if(args[0] === 'conf' && args[1] === 'casing') {
-    const config = fsExtra.readJSONSync(__dirname + '/codic-config.json')
-    config.casing = args[2]
-    fsExtra.writeJSONSync(__dirname + '/codic-config.json', config)
-
-/** 設定一覧の表示 */
-} else if (args[0] === 'conf' && args[1] === 'list') {
-    const config = fsExtra.readJsonSync(__dirname + '/codic-config.json')
-    console.log('access-token: ' + config['access-token'])
-    console.log('casing: ' + config.casing)
-
-/** ネーミングの取得 */
-} else if (args[0] === 'get') {
-    const config = fsExtra.readJsonSync(__dirname + '/codic-config.json')
+/** ネーミング取得 */
+const get = (confFile, hisFile, text, casing) => {
+    const config = fsExtra.readJsonSync(confFile)
     const accessToken = config['access-token']
 
-    let casing = 'camel'
-    if (args[2]) {
-        casing = args[2]
-    } else if (config.casing && config.casing !== '') {
+    if (!casing && config.casing) {
         casing = config.casing
+    } else if (!casing && !config.casing) {
+        casing = 'camel'
     }
 
     switch(casing) {
@@ -59,35 +59,38 @@ if (args[0] === 'conf' && args[1] === 'token') {
             'Authorization': 'Bearer ' + accessToken
         },
         qs: {
-            text: args[1],
+            text: text,
             casing: casing
         },
         json: true
     }, (err, req, data) => {
-        const history = fsExtra.readJSONSync(__dirname + '/codic-history.json')
+        const history = fsExtra.readJSONSync(hisFile)
         history.names.push({
-            'request': args[1],
+            'request': text,
             'response': data[0].translated_text
         })
-        fsExtra.writeJSONSync(__dirname + '/codic-history.json', history)
+        fsExtra.writeJSONSync(hisFile, history)
         console.log(data[0].translated_text)
     })
+}
 
-/** ネーミング履歴の削除 */
-} else if (args[0] === 'history' && args[1] === 'clear') {
-    fsExtra.writeJSONSync(__dirname + '/codic-history.json', {
-        'names': []
-    })
-
-/** ネーミング履歴を取得 */
-} else if (args[0] === 'history') {
-    const history = fsExtra.readJSONSync(__dirname + '/codic-history.json')
-    for (let i = 0; i < history.names.length; i++) {
-        console.log(i++ + ': ' + history.names[i].request + ' -> ' + history.names[i].response)
+/** 履歴 */
+const history = {
+    history: file => {
+        const history = fsExtra.readJSONSync(file)
+        for (let i = 0; i < history.names.length; i++) {
+            console.log(i + 1 + ': ' + history.names[i].request + ' -> ' + history.names[i].response)
+        }
+    },
+    clear: file => {
+        fsExtra.writeJSONSync(file, {
+            'names': []
+        })
     }
+}
 
-/** ヘルプの表示 */
-} else if (args[0] === 'help') {
+/** ヘルプ */
+const help = () => {
     console.log('codic conf token <your_access_token>')
     console.log('codic conf casing <casing>')
     console.log('codic conf list')
@@ -95,8 +98,29 @@ if (args[0] === 'conf' && args[1] === 'token') {
     console.log('codic history')
     console.log('codic history clear')
     console.log('codic help')
+}
 
-/** バージョンの表示 */
-} else if (args[0] === 'version') {
+/** バージョン */
+const version = () => {
     console.log('v' + pkg.version)
+}
+
+
+
+if (args[0] === 'conf' && args[1] === 'token') {
+    conf.token(__dirname + '/codic-config.json', args[2])
+} else if(args[0] === 'conf' && args[1] === 'casing') {
+    conf.casing(__dirname + '/codic-config.json', args[2])
+} else if (args[0] === 'conf' && args[1] === 'list') {
+    conf.list(__dirname + '/codic-config.json')
+} else if (args[0] === 'get') {
+    get(__dirname + '/codic-config.json', __dirname + '/codic-history.json', args[1], args[2])
+} else if (args[0] === 'history' && args[1] === 'clear') {
+    history.clear(__dirname + '/codic-history.json')
+} else if (args[0] === 'history') {
+    history.history(__dirname + '/codic-history.json')
+} else if (args[0] === 'help') {
+    help()
+} else if (args[0] === 'version') {
+    version()
 }
